8fa06df0278f8c2168dcaab4ced1c6a8d71a4922
pcruz <pablo.cruz2497@gmail.com>
2024-07-01 13:58:56 -0400

    Ordenado e indentado codigo y abierto repo publico en github

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
 //Activar tooltips de Bootstrap
 
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
