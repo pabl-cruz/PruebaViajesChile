b97c6c1cd6f2d153f61465c3b8a77cb24cc8a7bb
pcruz <pablo.cruz2497@gmail.com>
2024-07-01 13:33:02 -0400

    arreglados estilos y añadidos enlaces de redes sociales y tooltips

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+//Activar tooltips de Bootstrap
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
